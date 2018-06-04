655b23c72f65ab0bca3176ccd9448bc1fa471a2c
pinkVixenFights <pinkVixenFights@protonmail.com>
2018-06-04 06:58:42 +0000

    update work

--- build.js
+++ build.js
@@ -3,6 +3,7 @@ var markdown    = require('metalsmith-markdown');
 var layouts     = require('metalsmith-layouts');
 var permalinks  = require('metalsmith-permalinks');
 const assets    = require('metalsmith-assets');
+const inlinesource = require('metalsmith-inline-source');
 
 Metalsmith(__dirname)
 .metadata({
@@ -13,16 +14,19 @@ Metalsmith(__dirname)
 })
 .source('src')
 .destination('public')
-.clean(false)
-  .use(assets({
-    source: 'assets',
-    destination: '.'
-  }))
+.clean(true)
+.use(assets({
+  source: 'assets',
+  destination: '.'
+}))
 .use(markdown())
 .use(permalinks())
 .use(layouts({
   directory: 'layouts',
   engine: 'handlebars'
+}))
+.use(inlinesource({
+  compress: false
 }))
 .build(function(err, files) {
   if (err) { throw err; }
